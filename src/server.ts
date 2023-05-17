ebe23a2aa2e2a1e8d82ede0ffa95510b61c0d66e
imashiqe <imashiqe@gmail.com>
2023-05-17 12:08:35 +0600

    server connection enable

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express()
-const port = 3000
+const port = 5000;
+
+
+//database connection
+async function bootstrap() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
+    console.log(`Database connection successfull`);
+    } catch (err) {
+        console.log(`Faild To connect database`,err)
+    }
+}
+bootstrap();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is listening on port ${port}`)
 })
