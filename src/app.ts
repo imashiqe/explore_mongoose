fba83ceccf55d30e3f851132a228dda5fce675f7
imashiqe <imashiqe@gmail.com>
2023-05-19 01:07:01 +0600

    schema form mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application ,NextFunction,Request,Response } from 'express';
 //const app = express();
 import cors from 'cors';
+import { Schema ,model} from 'mongoose';
 
 const app: Application = express();
 //using cors
@@ -45,7 +46,77 @@ app.get('/', (req: Request, res: Response , next: NextFunction) => {
 
     }
 
-  })
+      //creating  schema using  interface
+  const userSchema = new Schema<IUser>({
+    id: { type: String,
+    required: true ,
+    unique: true,
+      },
+   role: {
+       type: String,
+       required: true,
+    },
+    password:{
+       type: String,
+       required: true,
+
+    },
+    name: {
+       firstName:{
+        type:String,
+        required: true,
+       },
+       middleName:{
+         type: String
+       },
+       lastName: {
+         type: String,
+         required: true,
+       }
+    },
+    dateOfBirth: {
+      type: String
+     
+    },
+    gender: {
+      type: String,
+      enum : ['male', 'female']
+    },
+    email: {
+       type: String,
+       required: true,
+    },
+    contactNo: {
+      type:String,
+      required: true,
+    },
+    // emergencyContact: {
+    //    type: Number,
+    //    required: true,
+    // },
+    presentAddress: {
+       type: String,
+       required: true,
+    },
+    permanentAddress: {
+       type: String,
+       required: true,
+    }
+   // name: { type: String, required: true },
+   // email: { type: String, required: true },
+    // avatar: String
+  });
+
+  });
+
+
+  
+  // 3. Create a Model.
+  // const User = model<IUser>('IUser', userSchema);
+  
+  // run().catch(err => console.log(err));
+  
+  // async function run() 
   
 
 
